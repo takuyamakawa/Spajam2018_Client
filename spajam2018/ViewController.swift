271d32bc87dd4f0cd8454b7e84fddb15b1247eb7
takuyamakawa <g2117052@fun.ac.jp>
2018-05-08 19:51:33 +0900

    create a project

---
+++ spajam2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spajam2018
+//
+//  Created by 山川拓也 on 2018/05/08.
+//  Copyright © 2018年 山川拓也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
